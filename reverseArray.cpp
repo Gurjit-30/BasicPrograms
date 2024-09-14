f5eebe2143d680732b088e71d0bc543eec21673c
Gurjit-30 <143714757+Gurjit-30@users.noreply.github.com>
2024-09-14 22:25:10 +0530

    Update reverseArray.cpp

--- reverseArray.cpp
+++ reverseArray.cpp
@@ -2,7 +2,7 @@
 #include<string>
 using namespace std;
 void rev(char* arr,int n){
-    for(int s=0,e=n-1;s<=e;s++,e--){
+    for(int s=0,e=n-1;s<e;s++,e--){
         swap(arr[s],arr[e]);
     }
     for(int i =0;i<n;i++){
