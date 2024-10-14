daa5afe498fce5182144ae6e075e105a4397fa0b
Gurjit Singh <gurjitsingh3028@gmail.com>
2024-10-14 19:29:25 +0530

    Changes to be committed: 	renamed:    linkedList/insertionAtHead.cpp -> linkedList/insertion.cpp 	renamed:    linkedList/insertionAtHead.exe -> linkedList/insertion.exe

--- linkedList/insertion.cpp
+++ linkedList/insertion.cpp
@@ -18,6 +18,12 @@ class Node{
 };
 void print(Node* head);
 void insertionAtHead(Node* head,int data){
+   if(head==NULL){//if ll is empty then head=tail=null
+    Node* newNode=new Node(data);
+    head=newNode;
+    //tail= newNode;
+    return;
+   }
     Node* ins=new Node(data);
     ins->next=head;
     head=ins;
@@ -29,6 +35,23 @@ void insertionAtTail(Node* tail,int data){
     tail->next=neww;
     tail=neww;
     print(neww);
+}
+void insertionAtAny(int data ,int pos ,Node* head,Node* tail){
+    if(head==NULL){
+        Node* nee=new Node(data);
+        head=nee;
+        tail=nee;
+        return;    }
+        Node* newn=new Node(data);
+        Node* temp =head;
+        for(int i=0;i<pos-1;i++){
+            temp=temp->next;
+
+
+        }
+        newn ->next=temp->next;
+        temp->next=newn;
+
 }
 void print(Node* head){
     Node* temp=head;
